7c0865113216976ada16937c00e15a61aa73639e
mikes28 <mikeskrisztian06@gmail.com>
2025-02-07 11:03:01 +0100

    Konyv class implementalas a Program Classba

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MikesKrisztian_osztalyokGittel;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Konyv konyv1 = new("A kis herceg", "Antoine de Saint-Exupéry", 1943, 96, 2500);
+        Konyv konyv2 = new("A kis herceg", "Antoine de Saint-Exupéry");
+
+        Console.WriteLine(konyv1);
+        Console.WriteLine(konyv2);
+
+        konyv1.Arnoveles(500);
+        Console.WriteLine(konyv1);
+
+        konyv1.Kedvezmeny(10);
+        Console.WriteLine(konyv1);
+
+        Console.WriteLine(konyv1.HanyOldalMaradt(50));
+        Console.WriteLine(konyv1.HanyOldalMaradt(100));
+    }
+}
